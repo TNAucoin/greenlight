e0298ffc37da8d047e1dbf28d9077489e28c63ae
Travis Aucoin <travisdevaucoin@gmail.com>
2023-12-07 20:20:17 -0600

    Add UnmarshalJSON method and refine error handling
    Added a new UnmarshalJSON method in runtime.go for better parsing of JSON values. Additionally, refined error handling to deal with invalid runtime format cases. The commit also includes necessary changes in movies.go, where the 'Runtime' structure is now capable of using UnmarshalJSON for JSON handling.

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -3,9 +3,12 @@ package data
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRuntimeFormat = fmt.Errorf("invalid runtime format")
 
 // MarshalJSON is a custom marshaler for the Runtime type
 // that formats the output to a string representation of
@@ -14,4 +17,21 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedStringValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquotedStringValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	intValue, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(intValue)
+	return nil
 }
