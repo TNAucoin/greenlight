7c8c32dea89f55661aab2b39a6e5d34279b73518
Travis Aucoin <travisdevaucoin@gmail.com>
2023-12-07 20:10:47 -0600

    Improve JSON read functionality and error handling
    The changes include enhanced JSON reading capabilities to handle limits, disallow unknown fields, and handle more errors. It now checks if the request body is too large, provides errors for unknown fields in the JSON body, and ensures the body has only a single JSON value.

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 type envelope map[string]any
@@ -37,12 +39,17 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	return nil
 }
 
-func (app *application) readJSON(w http.ResponseWriter, http *http.Request, dst any) error {
-	err := json.NewDecoder(http.Body).Decode(dst)
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
 		switch {
 
 		case errors.As(err, &syntaxError):
@@ -61,10 +68,19 @@ func (app *application) readJSON(w http.ResponseWriter, http *http.Request, dst 
 
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
 
 		default:
 			return err
 		}
+	}
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
 }
