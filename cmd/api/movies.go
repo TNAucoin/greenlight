ee2add7f0e62ad609b9360871b23501ef9935f60
Travis Aucoin <travisdevaucoin@gmail.com>
2023-12-09 08:34:13 -0600

    Add validation for movie input fields
    A new validator package was introduced to validate movie input fields such as title, year, runtime, and genres. This change enhances the robustness of the code by ensuring that only valid information is processed and helps to avoid potential errors related to data types and formats. In case of failed validation, a distinct response is invoked.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/tnaucoin/greenlight/internal/data"
+	"github.com/tnaucoin/greenlight/internal/validator"
 	"net/http"
 	"time"
 )
@@ -18,6 +19,25 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	// Validation checks for creating a movie record.
+	v.Check(input.Title != "", "title", "this field cannot be blank")
+	v.Check(len(input.Title) < 500, "title", "this field is too long (maximum is 500 characters)")
+	v.Check(input.Year != 0, "year", "this field cannot be blank")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(input.Runtime != 0, "runtime", "this field cannot be blank")
+	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(input.Genres != nil, "genres", "this field cannot be blank")
+	v.Check(len(input.Genres) >= 1, "genres", "you must specify at least one genre")
+	v.Check(len(input.Genres) <= 5, "genres", "you cannot specify more than five genres")
+	v.Check(validator.Unique(input.Genres), "genres", "you cannot specify duplicate genres")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
