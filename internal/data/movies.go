3ed659c641ae7c50bebcda59dfd37d63920d8369
Travis Aucoin <travisdevaucoin@gmail.com>
2023-12-09 09:14:33 -0600

    Refactor movie data validation functions
    Validation rules for movie data have been refactored into separate functions. This gives better visibility of each validation rule applied and makes code cleaner and easier for debugging and maintenance. Additionally, it enhances code reusability for title, year, runtime, and genres' validation.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,25 +6,41 @@ import (
 )
 
 type Movie struct {
-	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"-"`
-	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"`
-	Runtime   Runtime   `json:"runtime,omitempty"`
-	Genres    []string  `json:"genres,omitempty"`
-	Version   int32     `json:"version"`
+	ID        int64
+	CreatedAt time.Time
+	Title     string
+	Year      int32
+	Runtime   Runtime
+	Genres    []string
+	Version   int32
+}
+
+func validateTitle(v *validator.Validator, title string) {
+	v.Check(title != "", "title", "this field cannot be blank")
+	v.Check(len(title) < 500, "title", "this field is too long (maximum is 500 characters)")
+}
+
+func validateYear(v *validator.Validator, year int32) {
+	v.Check(year != 0, "year", "this field cannot be blank")
+	v.Check(year >= 1888, "year", "must be greater than 1888")
+	v.Check(year <= int32(time.Now().Year()), "year", "must not be in the future")
+}
+
+func validateRuntime(v *validator.Validator, runtime Runtime) {
+	v.Check(runtime != 0, "runtime", "this field cannot be blank")
+	v.Check(runtime > 0, "runtime", "must be a positive integer")
+}
+
+func validateGenres(v *validator.Validator, genres []string) {
+	v.Check(genres != nil, "genres", "this field cannot be blank")
+	v.Check(len(genres) >= 1, "genres", "you must specify at least one genre")
+	v.Check(len(genres) <= 5, "genres", "you cannot specify more than five genres")
+	v.Check(validator.Unique(genres), "genres", "you cannot specify duplicate genres")
 }
 
 func ValidateMovie(v *validator.Validator, m *Movie) {
-	v.Check(m.Title != "", "title", "this field cannot be blank")
-	v.Check(len(m.Title) < 500, "title", "this field is too long (maximum is 500 characters)")
-	v.Check(m.Year != 0, "year", "this field cannot be blank")
-	v.Check(m.Year >= 1888, "year", "must be greater than 1888")
-	v.Check(m.Year <= int32(time.Now().Year()), "year", "must not be in the future")
-	v.Check(m.Runtime != 0, "runtime", "this field cannot be blank")
-	v.Check(m.Runtime > 0, "runtime", "must be a positive integer")
-	v.Check(m.Genres != nil, "genres", "this field cannot be blank")
-	v.Check(len(m.Genres) >= 1, "genres", "you must specify at least one genre")
-	v.Check(len(m.Genres) <= 5, "genres", "you cannot specify more than five genres")
-	v.Check(validator.Unique(m.Genres), "genres", "you cannot specify duplicate genres")
+	validateTitle(v, m.Title)
+	validateYear(v, m.Year)
+	validateRuntime(v, m.Runtime)
+	validateGenres(v, m.Genres)
 }
